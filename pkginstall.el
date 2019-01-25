7cb462596f73882fe4f4a5020a08329f4a8cbd26
paulo <pauloroberto.21s@gmail.com>
2019-01-25 22:26:53 +0000

    uma pequena otimização para tornar o processo menos transicional

--- pkginstall.el
+++ pkginstall.el
@@ -1,4 +1,5 @@
 ; needs emacs25 in order to work
+(package-refresh-contents)
 
 (setq pkg-list '(moe-theme abyss-theme django-mode emmet-mode evil goto-chg helm-make helm helm-core async jedi auto-complete jedi-core epc ctable concurrent monokai-theme multi nlinum pdf-tools let-alist popup projectile pkg-info epl python-django python-environment deferred python-mode restart-emacs slime macrostep soothe-theme tablist undo-tree web-mode))
 
@@ -8,4 +9,3 @@
     (setq list (cdr list))))
 
 (inst-els pkg-list)
-
