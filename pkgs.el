59e1cf2c1a67dc2816ad8a8279538511a99a5a58
paulo <pauloroberto.21s@gmail.com>
2019-02-03 00:58:03 +0000

    atualizando pacotes

--- pkgs.el
+++ pkgs.el
@@ -1,5 +1,5 @@
 ; needs emacs25 in order to work
 (package-refresh-contents)
 
-(setq pkg-list '(smart-mode-line markdown-mode geiser lispy paredit elpy moe-theme abyss-theme emmet-mode evil goto-chg helm-make helm helm-core async jedi auto-complete jedi-core epc ctable concurrent monokai-theme multi nlinum pdf-tools let-alist popup projectile pkg-info epl deferred python-mode restart-emacs slime macrostep soothe-theme tablist undo-tree web-mode irony))
+(setq pkg-list '(spacemacs-theme powerline spaceline s markdown-mode geiser lispy paredit elpy abyss-theme emmet-mode evil goto-chg helm-make helm helm-core async jedi auto-complete jedi-core epc ctable concurrent monokai-theme multi nlinum pdf-tools let-alist popup projectile pkg-info epl deferred python-mode restart-emacs slime macrostep tablist web-mode irony))
 
